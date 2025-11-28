d3b09ac62fa49ab7fdf88ad8e1209a9f9eaf4e90
Kaikkinimet <virpi.hytonen@helsinki.fi>
2025-11-29 00:44:22 +0100

    OSA 9 virhekoodit

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def find_item():
 @app.route("/item/<int:item_id>")
 def show_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     return render_template("show_item.html", item=item)
 
 
@@ -56,6 +58,8 @@ def create_items():
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
 
@@ -65,6 +69,8 @@ def edit_item(item_id):
 @app.route("/update_item/<int:item_id>", methods=["POST"])
 def update_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
     title = request.form["title"]
@@ -80,6 +86,8 @@ def update_item(item_id):
 @app.route("/remove_item/<int:item_id>", methods=["GET", "POST"])
 def remove_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
 
